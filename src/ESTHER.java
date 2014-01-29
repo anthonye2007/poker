f9d536f6bcf08f2d5d818f1206ad955ae7e3086c
anthony_elliott <ellioaac@uni.edu>
2014-01-28 22:18:10 -0600

    Add always fold agent

--- src/ESTHER.java
+++ src/ESTHER.java
@@ -33,10 +33,11 @@ public class ESTHER {
          players[2] = new AgentRandomPlayer(2);
          players[3] = new AgentAlwaysRaise(1);
          players[4] = new AgentRandomPlayer(3);
-         //players[5] = new Morris();
+         players[5] = new Elliott();
+         
          
          //System.out.println("You will be player #6");
-         players[5] = new AgentHumanCommandLine();
+         //players[5] = new AgentHumanCommandLine();
 
          
         if (mode == 1) {
