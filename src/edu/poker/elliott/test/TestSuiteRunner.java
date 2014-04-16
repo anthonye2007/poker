c73f132ddba523f297c60fc22041513d8e7988d8
anthony_elliott <ellioaac@uni.edu>
2014-04-15 20:35:40 -0500

    Add test suite

---
+++ src/edu/poker/elliott/test/TestSuiteRunner.java
@@ -0,0 +1,22 @@
+package edu.poker.elliott.test;
+
+import junit.framework.JUnit4TestAdapter;
+import junit.framework.TestSuite;
+import org.junit.runner.RunWith;
+import org.junit.runners.AllTests;
+
+@RunWith(AllTests.class)
+public class TestSuiteRunner {
+    public static TestSuite suite() {
+        TestSuite suite = new TestSuite();
+
+        suite.addTest(new JUnit4TestAdapter(ElliottToolsTest.class));
+        suite.addTest(new JUnit4TestAdapter(ProbFlush.class));
+        suite.addTest(new JUnit4TestAdapter(ProbPairTest.class));
+        suite.addTest(new JUnit4TestAdapter(ProbStraightTest.class));
+        suite.addTest(new JUnit4TestAdapter(ProbThreeOfAKind.class));
+        suite.addTest(new JUnit4TestAdapter(ProbTwoPairTest.class));
+
+        return suite;
+    }
+}
